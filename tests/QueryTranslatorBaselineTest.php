bf7746a3dabc30a171355109da12f389e0863699
Dan Feder <dan.feder@civicactions.com>
2026-09-16 16:21:11 -0400

    Add phpunit and initial tests

---
+++ tests/QueryTranslatorBaselineTest.php
@@ -0,0 +1,145 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SqlParserTest\Tests;
+
+use PHPSQLParser\PHPSQLParser;
+use PHPUnit\Framework\TestCase;
+use SqlParserTest\QueryTranslator;
+
+final class QueryTranslatorBaselineTest extends TestCase
+{
+    private const RESOURCE_ID = '909ab5c6-54b6-40ac-96bc-f7198c9c734d';
+
+    public function testTranslatesSimpleSelectFromWhereOrderLimit(): void
+    {
+        $sql = sprintf(
+            'SELECT record_number FROM `%s` t WHERE (record_number = 1) ORDER BY record_number DESC LIMIT 5',
+            self::RESOURCE_ID
+        );
+
+        $payload = $this->translateSqlToPayload($sql);
+
+        self::assertSame(
+            [['resource' => 't', 'property' => 'record_number']],
+            $payload['properties']
+        );
+        self::assertSame(
+            [['id' => self::RESOURCE_ID, 'alias' => 't']],
+            $payload['resources']
+        );
+        self::assertSame(
+            [[
+                'resource' => 't',
+                'property' => 'record_number',
+                'operator' => '=',
+                'value' => 1,
+            ]],
+            $payload['conditions']
+        );
+        self::assertSame(
+            [['resource' => 't', 'property' => 'record_number', 'order' => 'desc']],
+            $payload['sorts']
+        );
+        self::assertSame(5, $payload['limit']);
+        self::assertSame(0, $payload['offset']);
+    }
+
+    public function testTranslatesAggregateCountWithAlias(): void
+    {
+        $sql = sprintf(
+            'SELECT COUNT(record_number) AS c FROM `%s` t',
+            self::RESOURCE_ID
+        );
+
+        $payload = $this->translateSqlToPayload($sql);
+
+        self::assertSame('count', $payload['properties'][0]['expression']['operator']);
+        self::assertSame('c', $payload['properties'][0]['alias']);
+        self::assertSame(
+            [['resource' => 't', 'property' => 'record_number']],
+            $payload['properties'][0]['expression']['operands']
+        );
+    }
+
+    public function testTranslatesArithmeticExpressionWithAlias(): void
+    {
+        $sql = sprintf(
+            'SELECT (record_number + 4) AS n FROM `%s` t',
+            self::RESOURCE_ID
+        );
+
+        $payload = $this->translateSqlToPayload($sql);
+
+        self::assertSame('+', $payload['properties'][0]['expression']['operator']);
+        self::assertSame('n', $payload['properties'][0]['alias']);
+        self::assertSame(
+            [
+                ['resource' => 't', 'property' => 'record_number'],
+                4,
+            ],
+            $payload['properties'][0]['expression']['operands']
+        );
+    }
+
+    public function testTranslatesInCondition(): void
+    {
+        $sql = sprintf(
+            'SELECT record_number FROM `%s` t WHERE record_number IN (1,2,3)',
+            self::RESOURCE_ID
+        );
+
+        $payload = $this->translateSqlToPayload($sql);
+
+        self::assertSame('in', $payload['conditions'][0]['operator']);
+        self::assertSame([1, 2, 3], $payload['conditions'][0]['value']);
+    }
+
+    public function testResourceOptionCreatesResourceWhenFromMissing(): void
+    {
+        $sql = 'SELECT record_number WHERE (record_number = 1)';
+
+        $payload = $this->translateSqlToPayload($sql, self::RESOURCE_ID);
+
+        self::assertSame(
+            [['id' => self::RESOURCE_ID, 'alias' => 't']],
+            $payload['resources']
+        );
+        self::assertSame('record_number', $payload['properties'][0]['property']);
+    }
+
+    public function testThrowsForUnparenthesizedBooleanWhereInCurrentBehavior(): void
+    {
+        $sql = sprintf(
+            'SELECT record_number FROM `%s` t WHERE record_number = 1 AND record_number > 0',
+            self::RESOURCE_ID
+        );
+
+        $this->expectException(\DomainException::class);
+        $this->expectExceptionMessage('Invalid mix of expressions. Try separating expressions with parentheses.');
+
+        $this->translateSqlToPayload($sql);
+    }
+
+    public function testThrowsForProhibitedClause(): void
+    {
+        $sql = sprintf(
+            'SELECT record_number FROM `%s` t GROUP BY record_number',
+            self::RESOURCE_ID
+        );
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Prohibited SQL clauses detected: GROUP');
+
+        $this->translateSqlToPayload($sql);
+    }
+
+    private function translateSqlToPayload(string $sql, ?string $resource = null): array
+    {
+        $parser = new PHPSQLParser($sql);
+        $query = QueryTranslator::translate($parser->parsed, $resource);
+
+        return json_decode($query->pretty(), true, 512, JSON_THROW_ON_ERROR);
+    }
+}
